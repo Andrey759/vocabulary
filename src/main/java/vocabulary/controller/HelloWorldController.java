9b303c77157ada232b67a7bc8a489992ddf28969
Andrey <andrey759@list.ru>
2023-06-05 20:56:25 +0300

    chat gpt

--- src/main/java/vocabulary/controller/HelloWorldController.java
+++ src/main/java/vocabulary/controller/HelloWorldController.java
@@ -1,17 +1,41 @@
 package vocabulary.controller;
 
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @Slf4j
 public class HelloWorldController {
 
     @GetMapping("/")
-    public ResponseEntity<String> helloWorld() {
-        log.info("GET /");
-        return ResponseEntity.ok("Hello World!");
+    //@ModelAttribute("aaa") Aaa aaa
+    public String helloWorld(Model model) {
+        model.addAttribute("text", "My text");
+        return "index";
+    }
+
+    @GetMapping("/api")
+    @ResponseBody
+    public ResponseEntity<String> api() {
+        String token = "";
+        OpenAiService service = new OpenAiService(token);
+
+        System.out.println("\nCreating completion...");
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .model("ada")
+                .prompt("Somebody once told me the world is gonna roll me")
+                .echo(false)
+                .user("anonim1234567890987654321")
+                .n(3)
+                .build();
+        service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
+
+        return ResponseEntity.ok("OK");
     }
 }
